b8a2eab43bb923b18039f1bb959ec97e5fd9e8d4
Priyank Shah <priyank2222@gmail.com>
2024-08-05 11:26:29 +0530

    Initial Commit

---
+++ src/main/java/com/xpert/TravellingAgency/TravellingAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.xpert.TravellingAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpert/TravellingAgency/TravellingAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpert.TravellingAgency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellingAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
